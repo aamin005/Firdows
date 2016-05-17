d770a637208ffe1c3b42d1e0b903ecfb83eec6ed
“aamin005” <aamin005@gmail.comssh-keygen -t rsa -C aamin005@gmail.com>
2016-05-18 01:08:01 +0700

    Add authorization to post creation

--- spec/features/user_creates_post_spec.rb
+++ spec/features/user_creates_post_spec.rb
@@ -1,7 +1,10 @@
 require "rails_helper"
 
 RSpec.describe "Creating post" do 
+	let(:user) {User.create(username: "exampleuser", email: "example@user.com", password: 'password', password_confirmation: 'password')}
+	
 	scenario "successfully" do 
+		sign_in user
 		visit root_path
 		click_on "Post a poster"
 
@@ -9,10 +12,14 @@ RSpec.describe "Creating post" do
 		fill_in "Body", with: "Some the best poster for your love"
 		click_on "Publish"
 
-		expect(page).to have_content "My first poster"
+		within(".posts") do
+			expect(page).to have_content "My first poster"
+			expect(page).to have_content "exampleuser"
+		end
 	end
 
 	scenario "unsuccessfully" do 
+		sign_in user
 		visit root_path
 		click_on "Post a poster"
 
@@ -21,5 +28,12 @@ RSpec.describe "Creating post" do
 		click_on "Publish"
 
 		expect(page).to have_css ".error"
+	end
+
+	scenario "non-logged in user connot create post" do
+		visit root_path
+		click_on "Post a poster"
+
+		expect(current_path).to eq(new_user_session_path)
 	end
 end
