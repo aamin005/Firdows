18599031c7f44b3fe5601cc6447a6e3e58095606
“aamin005” <aamin005@gmail.comssh-keygen -t rsa -C aamin005@gmail.com>
2016-05-23 18:03:10 +0700

    Allow users to add tags to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,23 @@ class Post < ActiveRecord::Base
 
 
 	belongs_to :user
+	has_many :taggings, dependent: :destroy
+	has_many :tags, through: :taggings
 
 	delegate :username, to: :user, allow_nil: true
+
+	def self.tagged_with(name)
+		Tag.find_by!(name: name).posts
+	end
+
+	def all_tags=(names)
+		# name = "Reciters, Quran"
+		self.tags = names.split(',').map do |name|
+			Tag.where(name: name).first_or_create!
+		end
+	end
+
+	def all_tags
+		tags.map(&:name).join(", ")
+	end
 end   
